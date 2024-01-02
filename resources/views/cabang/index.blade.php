1e388279eb5acad7ba940b54e6a5695786f82057
sahrullugianabadri <sahrullugianabadri61@gmail.com>
2024-01-02 19:32:10 +0700

    group_b

--- resources/views/cabang/index.blade.php
+++ resources/views/cabang/index.blade.php
@@ -1,46 +1,52 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Daftar Toko Cabang</title>
-</head>
-<body>
+<x-app-layout>
+    <x-slot name="hearder">
+        <h2 class="font-semibold text-xl text gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Cabang')}}
+        </h2>
+    </x-slot>
 
-    <h1>Daftar Toko Cabang</h1>
+    <div class="py-12">
+        <div class="mas-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
 
-    <table border="1">
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>Nama Toko</th>
-                <th>Nama Cabang</th>
-                <th>Alamat Cabang</th>
-                <th>Aksi</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach($cabangs as $cabang)
-                <tr>
-                    <td>{{ $cabang->id }}</td>
-                    <td>{{ $cabang->toko->nama }}</td>
-                    <td>{{ $cabang->nama }}</td>
-                    <td>{{ $cabang->alamat }}</td>
-                    <td>
-                        <a href="{{ route('cabang.show', $cabang->id) }}">Detail</a>
-                        <a href="{{ route('cabang.edit', $cabang->id) }}">Edit</a>
-                        <form action="{{ route('cabang.destroy', $cabang->id) }}" method="POST">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit">Hapus</button>
-                        </form>
-                    </td>
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
+                    <!--CONTENT HERE-->
 
-    <a href="{{ route('cabang.create') }}">Tambah Cabang</a>
+                    <x-table>
+                        <x-slot name="hearder">
+                            <tr>
+                                <th>#</th>
+                                <th>cabang</th>
+                                <th>id_cabang</th>
+                                <th>nama_cabang</th>
+                                <th>alamat</th>
+                                <th>telepon</th>
+                                <th>pegawai</th>
+                            </tr>
+                        </x-slot>
+                        @php $num=1;
+                        @endphp
 
-</body>
-</html>
+                        @foreach ($cabangs as $cabang )
+                        <tr>
+                            <td>{{ $num++ }} </td>
+                            <td>{{ $cabang->id_cabang }} </td>
+                            <td>{{ $cabang->nama_cabang}} </td>
+                            <td>{{ $cabang->alamat }} </td>
+                            <td>{{ $cabang->telepon }} </td> 
+                            <td>{{ $cabang->pegawai }} </td>
+                            <td>
+                                <img src="{{ asset('storage/cabang_alamat/'.$cabang->alamat) }}" width="100px"/>
+                            </td>
+                            <td>{{ $cabang->quantity }}</td>
+                            <td>{{ $cabang->cabangshelf->alamat }}-{{ $cabang->cabangshelf->name }}</td>
+                        </tr>    
+                        @endforeach
+                    </x-table>
+
+                        
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
